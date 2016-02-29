7c5584f8bbe03dd9c891979f46bc6f8a8efd31b0
Rafael Hengles <rhengles@gmail.com>
2016-02-29 17:04:04 -0300

    Build de html com Nunjucks

---
+++ tasks/includes/dir.js
@@ -0,0 +1,94 @@
+var fs = require('fs');
+var path = require('path');
+
+function dir(opt) {
+	function next(cbNext) {
+		function processFile(name) {
+			function sendFile() {
+				if ( opt.verbose ) {
+					console.log('sendfile', file);
+				}
+				return cbNext(null, file);
+			}
+			if ( opt.verbose ) {
+				console.log('processfile', name);
+			}
+			var file =
+				{ name: name
+				, stat: null
+				, dir: currentDir
+				};
+			if ( opt.rec || opt.stat ) {
+				var fpath = path.join(currentDir.name, currentDir.sub, name);
+				fs.stat(fpath, function(err, stat) {
+					if ( opt.verbose ) {
+						console.log('stat', err, stat);
+					}
+					if ( err ) {
+						return cbNext(err);
+					}
+					file.stat = stat;
+					var rec = false;
+					if ( opt.rec && stat.isDirectory() ) {
+						rec = ( opt.rec instanceof Function
+							? opt.rec(file, currentDir)
+							: true
+							);
+					}
+					if ( rec ) {
+						var sdir =
+							{ name: currentDir.name
+							, sub: path.join(currentDir.sub, name)
+							};
+						queue.push(sdir);
+						//return next(cbNext);
+					}
+					return sendFile(file);
+				});
+			} else {
+				return sendFile(file);
+			}
+		}
+		if ( opt.verbose ) {
+			console.log('next');
+		}
+		var currentFile = files.shift();
+		if ( opt.verbose ) {
+			console.log('currentfile', currentFile);
+		}
+		if ( currentFile ) {
+			return processFile(currentFile);
+		}
+		currentDir = queue.shift();
+		if ( null == currentDir ) {
+			if ( opt.verbose ) {
+				console.log('enddir');
+			}
+			return cbNext(null, null);
+		} else {
+			var cdir = path.join(currentDir.name, currentDir.sub);
+			fs.readdir(cdir, function(err, dirFiles) {
+				if ( opt.verbose ) {
+					console.log('readdir', err, dirFiles);
+				}
+				if ( err ) {
+					return cbNext(err);
+				}
+				files = dirFiles;
+				return next(cbNext);
+			});
+		}
+	}
+	var queue = [].concat(opt.path).map(function(name) {
+			return (
+				{ name: name
+				, sub: ''
+				});
+		});
+	var queueRec = [];
+	var currentDir;
+	var files = [];
+	return next;
+}
+
+module.exports = dir;
